1fe9a1017cfc8999b996e1bdeaa964595380afbc
cmdelaserna <carlosmartinezserna@gmail.com>
2015-03-15 01:19:55 -0700

    deleted all libraries

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
 
-window.sr = new scrollReveal( config );
-
-var config = {
-        mobile: true,
-        reset:  false,
-        delay:  'once',
-        vFactor: 0.10
-      };
-
 $( document ).ready(function() {
     $('body').fadeIn('slow');
 });
